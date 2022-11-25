2bfc04e6bf7f8106dbc4a62bea3316104c47b13d
Alif Yanuar Aditya <63470202+SerCycle@users.noreply.github.com>
2022-11-25 07:58:20 +0700

    bug

--- MODUL3 FIKRI/config/connector.php
+++ MODUL3 FIKRI/config/connector.php
@@ -1,5 +1,5 @@
 <?php
-$connector = new mysqli("localhost".":"."3315", "root", "", "modul3");
+$connector = new mysqli("localhost".":"."3315", "root", "1202204112", "modul3");
 
 if (!$connector) {
   die("connector$connector Gagal: " . $connector->connect_error);
