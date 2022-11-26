1a3f34ad86bc4ee8996b47a1ac198d973511fad4
fikrifath <fikrifathoni@student.telkomuniversity.ac.id>
2022-11-26 23:38:53 +0700

    Modul3_

---
+++ MODUL3 FIKRI/showroom_fikri_table.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3315
+-- Generation Time: Nov 26, 2022 at 05:37 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `modul3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `showroom_fikri_table`
+--
+
+CREATE TABLE `showroom_fikri_table` (
+  `id_mobil` int(255) NOT NULL,
+  `nama_mobil` varchar(255) NOT NULL,
+  `pemilik_mobil` varchar(255) NOT NULL,
+  `merk_mobil` int(255) NOT NULL,
+  `tanggal_beli` date NOT NULL,
+  `deskripsi` text NOT NULL,
+  `foto_mobil` varchar(255) NOT NULL,
+  `status_pembayaran` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `showroom_fikri_table`
+--
+
+INSERT INTO `showroom_fikri_table` (`id_mobil`, `nama_mobil`, `pemilik_mobil`, `merk_mobil`, `tanggal_beli`, `deskripsi`, `foto_mobil`, `status_pembayaran`) VALUES
+(1, 'waw', 'adada', 0, '0000-00-00', '  weaw  ', 'download.png', 'Lunas');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `showroom_fikri_table`
+--
+ALTER TABLE `showroom_fikri_table`
+  ADD PRIMARY KEY (`id_mobil`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `showroom_fikri_table`
+--
+ALTER TABLE `showroom_fikri_table`
+  MODIFY `id_mobil` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
