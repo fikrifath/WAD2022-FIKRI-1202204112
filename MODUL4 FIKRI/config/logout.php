52ed09102034bdf823dc88f2ad9faae6bb9c2740
Alif Yanuar Aditya <63470202+SerCycle@users.noreply.github.com>
2022-12-02 07:56:23 +0700

    bug

--- MODUL4 FIKRI/config/logout.php
+++ MODUL4 FIKRI/config/logout.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
-session_unset();
-session_destroy();
+session_unset()
 header("location:../index.php");
 setcookie('id', '', time() - 3600);
 setcookie('key', '', time() - 3600);
